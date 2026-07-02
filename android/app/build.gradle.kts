2a577bccc0a615a94531a77d3b3abb8167e734a4
heeyun <ato8757@gmail.com>
2026-07-02 23:38:51 +0900

    업데이트 1.0.1+12

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.heeyun.hangangbus"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.0"
+        versionCode = 12
+        versionName = "1.0.1"
         
         // Manifest 에러 해결용
         addManifestPlaceholders(mapOf("applicationName" to "android.app.Application"))
