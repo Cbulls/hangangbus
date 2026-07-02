090e63a431b671700a508aaa95ddd7c08065504d
heeyun <ato8757@gmail.com>
2026-07-02 21:41:36 +0900

    버전 11 업데이트

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         applicationId = "com.heeyun.hangangbus"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0"
         
         // Manifest 에러 해결용
