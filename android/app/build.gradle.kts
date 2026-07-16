095cd2a7838f4823f902e6b94e3600127848b702
Cbulls <ato8757@gmail.com>
2026-07-16 21:53:23 +0900

    디자인 수정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.heeyun.hangangbus"
         minSdk = 24
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.0.2"
+        versionCode = 17
+        versionName = "1.0.3"
         
         // Manifest 에러 해결용
         addManifestPlaceholders(mapOf("applicationName" to "android.app.Application"))
